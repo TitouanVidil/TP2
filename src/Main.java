4c763dac747a8d424f3c03fd23eec0f439251547
TitouanVidil <titouan.vidil@gmail.com>
2023-02-09 09:30:39 +0100

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
+        ArrayList<Humain> List =  new ArrayList<Humain>();
         Agrege a1 = new Agrege("Titouan",2000,"vert");
-        System.out.println(a1.getNom());
-        System.out.println(a1.getSalaire());
-        System.out.println(a1.getCouleur());
-        a1.setCouleurDefaut();
-        System.out.println(a1.getCouleur());
-        a1.meurt();
+        List.add(a1);
+        MaitreDeConference mdc1 = new MaitreDeConference("Gauthier",500,"orange");
+        List.add(mdc1);
+
+        for(Humain hum: List)
+        {
+            hum.meurt();
+        }
     }
 }
